a303bcdcc4fd00f19e93641b1497c3aa3f933ea3
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-02-15 16:18:16 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button, Card } from "@components";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Button>Bouton</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+/* import all components here */
+
+export { Button } from "./button";
+export { Card } from "./card";
